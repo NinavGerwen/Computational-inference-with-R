dfe30447147483190246a31452c645251f130d72
NinavGerwen <nina.van.gerwen@gmail.com>
2021-10-05 13:44:11 +0200

    Update NinavanGerwenAssignment1.R

--- NinavanGerwenAssignment1.R
+++ NinavanGerwenAssignment1.R
@@ -19,20 +19,23 @@ total_data <- cbind(mean_per_two_factors, variance_per_two_factors$x)
 ## Calculate the standard error for every group on every phase
 total_data$se <- sqrt(total_data$`variance_per_two_factors$x`) / sqrt(30)
 
-Profile_Plot <- ggplot(data = total_data, aes(x = Group.2, y = x, group = Condition, colour = Group.1)) +
+Profile_Plot <- ggplot(data = total_data, aes(x = Group.2, y = x, group = Group.1, colour = Group.1)) +
   ## create points for estimations for each group
   geom_point(shape = 18, size = 2) +
   ## create a line between all point estimations for each group
   geom_line(linetype = "longdash") + 
   ## create an interval for every point estimation in the graph through geom_errorbar
-  geom_errorbar(aes(ymin = x - se, ymax = x + se), width = .1) +
+  geom_errorbar(aes(ymin = x - 1.96*se, ymax = x + 1.96*se), width = .1) +
   ## apply every possible label to the graph, including legend label
   labs(x = "Phase", y = "Anxiety", title = "Figure 1", subtitle = "Anxiety as a function of time for two conditions", color = "Condition") +
   ## inverse the labels for the conditions in the legend
   guides(color = guide_legend(reverse = TRUE)) +
-  ## change the colours for the different conditions and apply a general theme
-  scale_colour_manual(values = c("#2166AC", "#B2182B")) + theme_bw() +
+  ## change the colours and labels for the different conditions and apply a general theme
+  scale_colour_manual(values = c("#2166AC", "#B2182B"), labels = c("Control", "Group Therapy")) + theme_bw() +
   ## position the legend on the top-right outside of the graph
-  theme(legend.justification = "top", plot.title = element_text(face = "bold"), plot.subtitle = element_text(face = "italic"))
+  theme(legend.justification = "top", plot.title = element_text(face = "bold"), plot.subtitle = element_text(face = "italic")) +
+  ## change the labels on the x-axis
+  scale_x_discrete(limits = c("pretest", "posttest", "followup"), labels = c("Pretest", "Posttest", "Follow-up"))
 
 Profile_Plot
+
